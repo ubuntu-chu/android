449d40d4b32bfb3b604f71081fdfa0109c2aacba
chum <chum@gmail.com>
2015-05-07 16:31:44 +0800

    init

---
+++ adb-push-modules.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+lcd_key=lcd
+ctp_key=ctp
+sysconfig_key=sysconfig
+pmu_key=pmu
+wait_any_key_reboot=0
+
+help(){
+	echo "Usage                 : $0 [$lcd_key|$ctp_key|$sysconfig_key|$pmu_key]"
+	echo "Param $lcd_key|$ctp_key|$sysconfig_key|$pmu_key"
+	exit 1
+
+} 
+
+execute_cmd()
+{
+	echo "$@"
+	$@
+	#if [ $? -ne 0   ];then
+	#	echo "execute $@ failed! please check what happened!"
+	#	exit 1
+	#fi
+}
+
+is_module_insmod()
+{
+	execute_cmd adb shell lsmod | grep -q $1
+	return $?
+}
+
+module_insmod()
+{
+	execute_cmd adb shell insmod $1
+	return $?
+}
+
+module_rmmod()
+{
+	execute_cmd adb shell rmmod $1
+	return $?
+}
+
+if [ $# -ne 1 ]; then
+	help
+fi
+
+case $1 in
+	$lcd_key)
+		MODULE_PATH=/media/buildserver/android/lichee/linux-3.4/drivers/video/sunxi/
+		cd $MODULE_PATH
+
+		module_list="disp/disp.ko lcd/lcd.ko hdmi/hdcp.ko"
+
+		execute_cmd adb remount
+		for module in $module_list; do
+			execute_cmd adb push $module /vendor/modules/$module
+			execute_cmd adb shell chmod 600 /vendor/modules/$module
+		done
+		;;
+
+	$ctp_key)
+		MODULE_PATH=/media/buildserver/android/lichee/linux-3.4/drivers/input/touchscreen/gslx680
+		cd $MODULE_PATH
+
+		execute_cmd adb remount
+		execute_cmd adb push gslX680.ko /vendor/modules/gslX680.ko
+		execute_cmd adb shell chmod 600 /vendor/modules/gslX680.ko
+		;;
+	$sysconfig_key)
+		SYSCONFIG_BIN=/media/buildserver/android/lichee/tools/pack/out/sys_config.bin
+		MOUNT_POINT="/data/bootloader"
+		execute_cmd adb shell "mkdir $MOUNT_POINT"
+		execute_cmd adb shell "mount -t vfat /dev/block/by-name/bootloader  $MOUNT_POINT"
+		execute_cmd adb push $SYSCONFIG_BIN $MOUNT_POINT/script.bin
+		execute_cmd adb shell umount $MOUNT_POINT
+		;;
+	$pmu_key)
+		MODULE_PATH=/media/buildserver/android/lichee/linux-3.4/drivers/power/axp_power
+		MODULE_ANDROID_PATH=/vendor/modules
+		cd $MODULE_PATH
+
+		module_list="virtual15.ko virtual15_dev.ko virtual22.ko virtual22_dev.ko"
+
+		execute_cmd adb remount
+		for module in $module_list; do
+			execute_cmd adb push $module $MODULE_ANDROID_PATH/$module
+			execute_cmd adb shell chmod 600 $MODULE_ANDROID_PATH/$module
+		done
+
+		echo ""
+		echo ""
+
+		for module in $module_list; do
+			module_rmmod=${module%.ko}
+			is_module_insmod $module_rmmod
+			if [ $? -eq 0 ]; then
+				module_rmmod $MODULE_ANDROID_PATH/$module_rmmod
+			fi
+			module_insmod $MODULE_ANDROID_PATH/$module
+		done
+		#for module in $module_list; do
+		#	echo "check module($module) is ismod"
+		#	is_module_insmod $module 
+		#	if [ $? -eq 0 ]; then
+		#		echo "module($module) rmmod"
+		#		module_rmmod $module
+		#	fi
+		#	echo "module($module) insmod"
+		#	module_insmod $1
+		#done
+		exit 0
+		;;
+	*)
+		help
+		;;
+esac
+
+adb shell sync
+
+if [ $wait_any_key_reboot -eq 1 ]; then
+	echo press any key to reboot
+	read anykey
+fi
+
+adb reboot
+
---
+++ adb-regulator.sh
@@ -0,0 +1,175 @@
+#!/bin/sh
+
+#####!/sbin/busybox sh
+
+summary_key=summary
+set_key=set
+close_key=close
+info_key=info
+help_key=help
+lookup_key=whois
+close_key=close
+list_key=list
+
+ubuntu_key="ubuntu"
+android_key="android"
+run_env=$ubuntu_key
+#run_env=$android_key
+
+dump_tmp_file="./.regulator_dump"
+regulator_class_path=/sys/class/regulator
+regulator_device_path=/sys/devices/platform
+	
+help()
+{
+	echo "Usage                 : $0 [$summary_key|$set_key|$lookup_key|$help_key|$info_key|$list_key|$close_key]"
+	echo "Param $summary_key: list regulators status"
+	echo "Param $set_key: <regulator_name> <uV>"
+	echo "Param $info_key: <regulator_name>"
+	echo "Param $lookup_key: <regulator_name-SUPPLY>"
+	echo "Param $list_key: list regulators name in system"
+	echo "Param $close_key: <regulator_name> close regulators output"
+	echo ""
+
+	exit 1
+} 
+
+execute_cmd()                                                                                                              
+{
+    echo "$@"
+    $@
+    #if [ $? -ne 0    ];then
+    #   echo "execute $@ failed! please check what happened!"
+    #   exit 1
+    #fi
+
+}
+
+name_convert()
+{
+	echo $1 | grep -q "axp806"
+	if [ $? -eq 0 ]; then
+		regulator_input=`echo $1  | sed -n "s/axp806_/reg-15-cs-/p"`
+	fi
+	echo $1 | grep -q "axp809"
+	if [ $? -eq 0 ]; then
+		regulator_input=`echo $1  | sed -n "s/axp809_/reg-22-cs-/p"`
+	fi
+	#echo $1 | sed -n "s/axp809_/reg-22-cs-/p"
+	#echo $1 | sed -n "s/axp806_/reg-15-cs-/p"
+	#regulator_input=`echo $1 | sed -en "s/axp809_/reg-22-cs-/gp;s/axp806_/reg-15-cs-/gp"`
+	echo $regulator_input
+}
+
+set_volt()
+{
+#	regulator_input=$1
+	name_convert $1
+	volt=$2
+	dev_attr_file=min_microvolts
+	if [ $run_env = $ubuntu_key ]; then
+		echo adb shell "echo $volt > $regulator_device_path/$regulator_input/$dev_attr_file"
+		adb shell "echo $volt > $regulator_device_path/$regulator_input/$dev_attr_file"
+		#设置电压时 程序内部有最大 最小值校验机制
+		#if [ $? -eq 0 ]; then
+		#	echo "volt: $volt modify success!"
+		#else
+		#	echo "volt: $volt modify failed!"
+		#fi
+	else
+		echo "echo $volt > $regulator_device_path/$regulator_input/$dev_attr_file"
+		echo $volt > $regulator_device_path/$regulator_input/$dev_attr_file
+	fi
+}
+
+if [ $run_env != $ubuntu_key -a $run_env != $android_key ]; then
+	echo "run_env must be $ubuntu_key or $android_key"
+	exit 1
+fi
+
+if [ $# -eq 0 ]; then
+	help
+fi
+
+case $1 in
+	$list_key)
+		if [ $run_env = $ubuntu_key ]; then
+			adb shell "busybox find  /sys/devices/platform/ -name "reg-*" | busybox grep -v reg-dummy | busybox cut -d / -f 5 | busybox sed -e \"s/reg-22-cs-/axp809_/g;s/reg-15-cs-/axp806_/g\""
+		else
+			find  /sys/devices/platform/ -name "axp8*_*" | cut -d / -f 5 | sed -e "s/reg-22-cs-/axp809_/g;s/reg-15-cs-/axp806_/g" 
+		fi
+		;;
+
+	$summary_key)
+		#打印系统中当前的 supply 与 consumer 列表
+		#adb shell "cat /d/regulator/supply_map"
+		#打印系统中当前的 supply 列表
+		echo ""
+		echo "name      enable/disable    use_count     voltage     slave_supply_list"
+		echo ""
+
+		#adb shell "cat /sys/class/regulator/dump" > $dump_tmp_file
+		#sed -n "regulator.*-SUPPLY/p" $dump_tmp_file
+
+		if [ $run_env = $ubuntu_key ]; then
+			adb shell cat $regulator_class_path/dump | sed -e "s/axp22/axp809/g;s/axp15/axp806/g"
+			#adb shell "cat $regulator_class_path/dump"
+		else
+			cat $regulator_class_path/dump | sed -e "s/axp22/axp809/g;s/axp15/axp806/g"
+		fi
+		;;
+	
+	$lookup_key)
+		if [ $# -ne 2 ]; then
+			help
+		fi
+		regulator_input=$2
+		regulator_label=${regulator_input%-SUPPLY}
+		regulator_label=$(echo $regulator_label | tr '[A-Z]' '[a-z]') 
+
+		if [ $run_env = $ubuntu_key ]; then
+			regulator_name=`adb shell cat $regulator_class_path/$regulator_label/name`
+		else
+			regulator_name=`cat $regulator_class_path/$regulator_label/name`
+		fi
+		echo "$regulator_label = $regulator_name" 
+		;;
+
+	$set_key)
+		if [ $# -ne 3 ]; then
+			help
+		fi
+		echo $3 | grep -q "[a-z,A-Z]"
+		if [ $? -eq 0 ]; then
+			echo "$3 contain char!"
+			exit 1
+		fi
+		set_volt $2 $3
+		;;
+
+	$close_key)
+		if [ $# -ne 2 ]; then
+			help
+		fi
+		set_volt $2 0
+		;;
+
+	$info_key)
+		if [ $# -ne 2 ]; then
+			help
+		fi
+		dev_attr_file=info
+		name_convert $2
+		if [ $run_env = $ubuntu_key ]; then
+			execute_cmd adb shell cat $regulator_device_path/$regulator_input/$dev_attr_file | sed -e "s/axp22/axp809/g;s/axp15/axp806/g"
+		else
+			execute_cmd cat $regulator_device_path/$regulator_input/$dev_attr_file | sed -e "s/axp22/axp809/g;s/axp15/axp806/g"
+		fi
+		;;
+
+	*)
+		help
+		;;
+esac
+
+
